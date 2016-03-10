a74ca970df93d294b11c03d6f58bca527c72685a
csc103 student <ychang000@citymail.cuny.edu>
2016-03-10 13:21:13 -0500

    jfljgdfg

--- wordcount.cpp
+++ wordcount.cpp
@@ -35,12 +35,21 @@ int main()
   string line;
 
  int numlines = 0;
+ set <string> lines //gettting a set for the lines
  int numchar = 0;
+ int unqlines = 0;
  while(getline(cin,line)) { //getline turns the paragraph into individual lines; cin,line gets the input from string line, which is the textfile
   numlines++; //increments the # of lines everytime it loops
   cout << "Number of lines " << numlines << endl; //number of lines
   numchar += line.length; //counts the characters in each line
   cout << "Number of characters " << numchar << endl; //number of characters in each line
-
+  lines.insert(lines); //insert lines to a set
+  set <string> it = lines.begin();
+  while (lines != lines.end ()) //use iterator
+   {
+    cout << *it << " ";
+    ++it;
+   unqlines = // how do convert the product of the set to numbers?
+  }
   return 0;
 }
